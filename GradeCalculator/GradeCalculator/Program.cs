82307dfaa3c314d1ce339ddfa61db86b2843e47a
heisqueenson9 <equeensoneduafo@gmail.com>
2025-08-01 01:28:07 -0700

    Added Grade Calculator

---
+++ GradeCalculator/GradeCalculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GradeCalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GRADE CALCULATOR");
+            Console.Write("Please enter a score between 0 and 100: ");
+
+            if (int.TryParse(Console.ReadLine(), out int score))
+            {
+                string grade = GetLetterGrade(score);
+                Console.WriteLine($"Your letter grade is: {grade}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+            }
+        }
+
+        static string GetLetterGrade(int score)
+        {
+            if (score >= 90)
+                return "A";
+            else if (score >= 80)
+                return "B";
+            else if (score >= 70)
+                return "C";
+            else if (score >= 60)
+                return "D";
+            else
+                return "F";
+        }
+    }
+}
